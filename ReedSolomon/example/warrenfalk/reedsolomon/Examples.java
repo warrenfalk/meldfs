b020c107ab90b60bd3290ad670000509bddf2ed7
Warren Falk <warren@warrenfalk.com>
2012-09-08 22:29:02 -0400

    Updated reed solomon example code

--- ReedSolomon/example/warrenfalk/reedsolomon/Examples.java
+++ ReedSolomon/example/warrenfalk/reedsolomon/Examples.java
@@ -1,5 +1,7 @@
 package warrenfalk.reedsolomon;
 
+import java.nio.ByteBuffer;
+
 import warrenfalk.reedsolomon.ReedSolomonCodingDomain.Coder;
 
 /**
@@ -11,42 +13,62 @@ public class Examples {
 	public static void main(String[] args) {
 		// Construct a coding domain that uses 8 symbols with 5 data symbols and 3 checksum symbols that can survive the loss of any 3
 		ReedSolomonCodingDomain domain = new ReedSolomonCodingDomain(5, 3);
-		// construct an empty code word
-		int[] word = new int[8];
-		// fill in the first five bytes (note that 8 bits is all that is allowed per symbol in the default coding domain's GaloisField, GF256)
-		word[0] = 0x03;
-		word[1] = 0x14;
-		word[2] = 0x15;
-		word[3] = 0x92;
-		word[4] = 0x65;
-		System.out.println("Code word with data symbols:");
-		dumpWord(word);
+		// construct test buffers (of just one byte)
+		// fill in the data bytes (note that 8 bits is all that is allowed per symbol in the default coding domain's GaloisField, GF256)
+		ByteBuffer[] words = new ByteBuffer[] {
+				ByteBuffer.wrap(new byte[] { 0x03 }),
+				ByteBuffer.wrap(new byte[] { 0x14 }),
+				ByteBuffer.wrap(new byte[] { 0x15 }),
+				ByteBuffer.wrap(new byte[] { 0x00 }),
+				ByteBuffer.wrap(new byte[] { 0x65 }),
+
+				ByteBuffer.wrap(new byte[] { 0x00 }),
+				ByteBuffer.wrap(new byte[] { 0x00 }),
+				ByteBuffer.wrap(new byte[] { 0x00 }),
+		};
+		System.out.println("Buffers with data symbols:");
+		dumpBuffers(words);
 		// get a coder that can calculate the checksums
 		Coder coder = domain.getChecksumCoder();
 		// calculate the checksum symbols
-		coder.calculate(word);
-		System.out.println("Code word with checksum symbols added:");
-		dumpWord(word);
+		coder.calculate(words);
+		System.out.println("Buffers with checksum symbols added:");
+		dumpBuffers(words);
 		// now erase two words and one checksum
-		word[2] = 0;
-		word[4] = 0;
-		word[7] = 0;
-		System.out.println("Code word with erasures:");
-		dumpWord(word);
+		words[2].put(0, (byte)0);
+		words[4].put(0, (byte)0);
+		words[7].put(0, (byte)0);
+		System.out.println("Buffers with simulated erasures:");
+		dumpBuffers(words);
 		// create a bitmask indicating which are valid and construct a recovery coder from that
 		int valid = ~((1 << 2) | (1 << 4) | (1 << 7));
 		Coder recoverer = domain.createCoder(valid);
-		recoverer.calculate(word);
+		recoverer.calculate(words);
 		// and they are recovered.
 		System.out.println("Reconstructed code word:");
-		dumpWord(word);
+		dumpBuffers(words);
 	}
 	
-	private static void dumpWord(int[] word) {
+	private static void dumpBuffers(ByteBuffer[] words) {
 		System.out.print("[ ");
-		for (int i = 0; i < word.length; i++)
-			System.out.print(" " + word[i] + " ");
+		for (int i = 0; i < words.length; i++)
+			words[i].clear();
+		boolean hasRemaining = true;
+		for (;hasRemaining;) {
+			hasRemaining = false;
+			for (int i = 0; i < words.length; i++) {
+				System.out.print(" " + hex(words[i].get()) + " ");
+				if (words[i].hasRemaining())
+					hasRemaining = true;
+			}
+		}
 		System.out.println(" ]");
 	}
 
+	final static char[] hex = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+	static String hex(byte b) {
+		return "" + hex[(b & 0xf0) >> 4] + hex[b & 0xf];
+	}
+	
+	
 }
