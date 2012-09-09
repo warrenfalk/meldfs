2a84c6a0be019509986cc84baab9f4c93f794522
Warren Falk <warren@warrenfalk.com>
2012-09-08 22:20:44 -0400

    Updated reed solomon unit test

--- ReedSolomon/test/warrenfalk/reedsolomon/TestReedSolomonCoding.java
+++ ReedSolomon/test/warrenfalk/reedsolomon/TestReedSolomonCoding.java
@@ -39,6 +39,11 @@ public class TestReedSolomonCoding {
 	public void tearDown() throws Exception {
 	}
 	
+	final static char[] hex = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+	static String hex(byte b) {
+		return "" + hex[(b & 0xf0) >> 4] + hex[b & 0xf];
+	}
+	
 	@Test
 	public void testCalculateSingleStripe() {
 		int dataSize = 4;
@@ -46,12 +51,12 @@ public class TestReedSolomonCoding {
 		int wordSize = dataSize + checksumSize;
 		
 		ByteBuffer[] columns = new ByteBuffer[] {
-				ByteBuffer.wrap("abc".getBytes()),
-				ByteBuffer.wrap("xyz".getBytes()),
-				ByteBuffer.wrap("012".getBytes()),
-				ByteBuffer.wrap("--".getBytes()),
-				ByteBuffer.wrap(new byte[3]),
-				ByteBuffer.wrap(new byte[3]),
+				ByteBuffer.wrap("abcdefghijklmnop".getBytes()),
+				ByteBuffer.wrap("ABCDEFGHIJKLMNOP".getBytes()),
+				ByteBuffer.wrap("01234567890!@#$%".getBytes()),
+				ByteBuffer.wrap(":-) :-( ;-) 8^) ".getBytes()),
+				ByteBuffer.wrap(new byte[16]),
+				ByteBuffer.wrap(new byte[16]),
 		};
 		
 		assert(columns.length == wordSize);
@@ -60,7 +65,7 @@ public class TestReedSolomonCoding {
 		Coder coder = domain.getChecksumCoder();
 		
 		long size = coder.calculate(columns);
-		assertEquals("size result of checksum calculation", 3 * checksumSize, size);
+		assertEquals("size result of checksum calculation", columns[0].limit() * checksumSize, size);
 		
 		// now simulate various failure scenarios and verify recoverability
 		ByteBuffer[] recovered = new ByteBuffer[columns.length];
@@ -80,7 +85,7 @@ public class TestReedSolomonCoding {
 			
 			for (int i = 0; i < wordSize; i++) {
 				if (0 == (validMask & (1 << i)))
-					recovered[i] = ByteBuffer.allocate(3);
+					recovered[i] = ByteBuffer.allocate(columns[0].limit());
 				else
 					recovered[i] = columns[i].duplicate();
 			}
@@ -88,12 +93,12 @@ public class TestReedSolomonCoding {
 			Coder recoverer = domain.createCoder(validMask);
 			size = recoverer.calculate(recovered);
 			
-			assertEquals("size result of recovery calculation for valid mask " + validMask, invalidCount * 3, size);
+			assertEquals("size result of recovery calculation for valid mask " + validMask, invalidCount * columns[0].limit(), size);
 			
 			for (int i = 0; i < wordSize; i++) {
 				assertContentEqual("content of recovery buffer " + i + " for valid mask " + validMask, columns[i], recovered[i]);
 			}
-		}			
+		}
 	}
 
 	private void assertContentEqual(String message, ByteBuffer expected, ByteBuffer actual) {
@@ -101,98 +106,6 @@ public class TestReedSolomonCoding {
 		int limit = Math.min(expected.limit(), actual.limit());
 		for (int i = 0; i < limit; i++)
 			assertEquals(message + ", byte at position " + i, expected.get(i), actual.get(i));
-	}
-
-	@Test
-	public void test() {
-		int[] word = new int[wordSize];
-		int[] wordIn = new int[wordSize];
-		int[] wordExp = new int[wordSize];
-		
-		// the data starts out with all random data, including the checksum symbols, but we need to run the calculation, first, to create checksum symbols
-		for (int offset = 0; offset < data.length; offset += wordSize) {
-			// copy the data bytes from the buffer to the word
-			for (int i = 0; i < dataSize; i++)
-				word[i] = data[offset + i] & gf.mask;
-			// calculate the checksum bytes
-			domain.getChecksumCoder().calculate(word);
-			// now put the checksum bytes into the random data
-			for (int i = 0; i < checksumSize; i++) {
-				int checksum = word[dataSize + i];
-				// we're using an 8-bit galois field, so none of the words should exceed 8 bits.
-				assertTrue("Checksum symbol fits within Galois Field length", checksum < gf.size);
-				data[offset + dataSize + i] = (byte)checksum;
-			}
-		}
-		
-		// now prepare to simulate lost data and recalculate
-		// we want to simulate every possible scenario, so loop through them
-		for (long validMask = (1 << wordSize) - 1; validMask > 0; validMask--) {
-			// If more than "checksumSize" bits in "validMask" are 0, then we can't recover in this scenario
-			int invalidCount = 0;
-			for (int b = 0; b < wordSize; b++) {
-				if (0 == (validMask & (1 << b)))
-					invalidCount++;
-			}
-			if (invalidCount == 0)
-				continue; // nothing to check
-			if (invalidCount > checksumSize)
-				continue; // not a possible recovery scenario
-			//System.out.println("Checking " + formatBinary(validMask) + " (" + validMask + ")");
-			Coder recoverer = domain.createCoder(validMask);
-			for (int offset = 0; offset < data.length; offset += wordSize) {
-				// read the word from the data except for the symbols that are not valid
-				for (int i = 0; i < wordSize; i++) {
-					wordExp[i] = data[offset + i] & 0xFF;
-					wordIn[i] = word[i] = ((validMask & (1 << i)) != 0) ? wordExp[i] : -1;
-				}
-				// attempt to recalculate
-				recoverer.calculate(word);
-				// make sure that only requested symbols were modified
-				for (int i = 0; i < wordSize; i++) {
-					if (0 != ((1 << i) & validMask))
-						assertEquals("Coder.calculate() should not modify unrequested symbols", wordIn[i], word[i]);
-					else
-						assertEquals((offset / wordSize) + "th word: n:m of " + dataSize + ":" + checksumSize + " before/after check on symbol #" + i + " for code word " + formatWord(wordExp) + " with valid mask of " + formatBinary(validMask) + " (" + validMask + ")", wordExp[i], word[i]);
-				}
-			}
-		}
-		
-		// now do single byte replacements, recalculating checksums
-		for (int i = 0; i < dataSize; i++) {
-			wordExp[i] = i;
-		}
-		domain.getChecksumCoder().calculate(wordExp);
-		for (int offset = 0; offset < data.length; offset += wordSize) {
-			for (int i = 0; i < wordSize; i++) {
-				wordIn[i] = data[offset + i] & 0xFF;
-			}
-			for (int d = 0; d < dataSize; d++) {
-				domain.recalcChecksum(wordIn, dataSize, d, wordIn[d], wordExp[d]);
-			}
-			for (int c = 0; c < checksumSize; c++) {
-				assertEquals(wordExp[dataSize + c], wordIn[dataSize + c]);
-			}
-		}
-	}
-
-	private String formatBinary(long validMask) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = wordSize - 1; i >= 0; i--)
-			sb.append((0 == (validMask & (1 << i))) ? '0' : '1');
-		return sb.toString();
-	}
-
-	private String formatWord(int[] word) {
-		StringBuilder sb = new StringBuilder();
-		sb.append('{');
-		for (int i = 0; i < wordSize; i++) {
-			if (i != 0)
-				sb.append(',');
-			sb.append(word[i]);
-		}
-		sb.append('}');
-		return sb.toString();
 	}
 
 }
